41a7704a6ff8fb58afdee4f40b36d9af26f5dcf4
Bilgehan KALKAN <bilgehan.kalkan@consultant.turkcell.com.tr>
2019-01-09 14:56:18 +0300

    Response will be written pretty. Response text can be selectable and can be copy into clip board.

--- Andzu/src/main/java/com/canakkoca/andzu/activities/NetworkLogDetailActivity.java
+++ Andzu/src/main/java/com/canakkoca/andzu/activities/NetworkLogDetailActivity.java
@@ -1,20 +1,25 @@
 package com.canakkoca.andzu.activities;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.canakkoca.andzu.R;
 import com.canakkoca.andzu.base.BaseActivity;
 import com.canakkoca.andzu.base.NetworkLog;
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -25,12 +30,27 @@ import java.util.Locale;
  */
 public class NetworkLogDetailActivity extends BaseActivity {
 
+    TextView date, url, code, latency, headers, postData, response;
+    Button shareInfo, copyResponse;
+    ImageView status;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_networklogdetail);
 
-        if(getSupportActionBar() != null) {
+        date = findViewById(R.id.date);
+        url = findViewById(R.id.url);
+        code = findViewById(R.id.code);
+        latency = findViewById(R.id.latency);
+        headers = findViewById(R.id.headers);
+        postData = findViewById(R.id.postData);
+        response = findViewById(R.id.response);
+        shareInfo = findViewById(R.id.shareInfo);
+        copyResponse = findViewById(R.id.button_copy_response);
+        status = findViewById(R.id.code_img);
+
+        if (getSupportActionBar() != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
             getSupportActionBar().setDisplayShowHomeEnabled(true);
         }
@@ -39,48 +59,54 @@ public class NetworkLogDetailActivity extends BaseActivity {
 
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault());
 
-        TextView date =  findViewById(R.id.date);
-        TextView url = findViewById(R.id.url);
-        TextView code = findViewById(R.id.code);
-        ImageView status = findViewById(R.id.code_img);
-        TextView latency = findViewById(R.id.latency);
-        TextView headers = findViewById(R.id.headers);
-        TextView postData = findViewById(R.id.postData);
-        TextView response = findViewById(R.id.response);
-
         date.setText(dateFormat.format(new Date(networkLog.getDate())));
         url.setText("[" + networkLog.getRequestType() + "] " + networkLog.getUrl());
         code.setText(networkLog.getResponseCode());
         latency.setText(String.format("%sms", networkLog.getDuration().intValue()));
         headers.setText(networkLog.getHeaders());
         postData.setText(networkLog.getPostData());
-        response.setText(networkLog.getResponseData());
+
+        try {
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            JsonParser jp = new JsonParser();
+            JsonElement je = jp.parse(networkLog.getResponseData());
+            response.setText(gson.toJson(je));
+        } catch (Exception e) {
+            response.setText(networkLog.getResponseData());
+        }
         response.setTextSize(15);
         postData.setText(networkLog.getPostData());
 
-        if(networkLog.getResponseCode().startsWith("2")){
+        if (networkLog.getResponseCode().startsWith("2")) {
             status.setBackgroundColor(Color.GREEN);
             code.setTextColor(Color.GREEN);
-        }else if(networkLog.getResponseCode().startsWith("4")) {
+        } else if (networkLog.getResponseCode().startsWith("4")) {
             status.setBackgroundColor(Color.parseColor("#ffa500"));
             code.setTextColor(Color.parseColor("#ffa500"));
-        }else if(networkLog.getResponseCode().startsWith("5")) {
+        } else if (networkLog.getResponseCode().startsWith("5")) {
             status.setBackgroundColor(Color.RED);
             code.setTextColor(Color.RED);
         }
 
-        Button btn = findViewById(R.id.shareInfo);
-        btn.setOnClickListener(new View.OnClickListener() {
+        shareInfo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent sendIntent = new Intent();
                 sendIntent.setAction(Intent.ACTION_SEND);
                 sendIntent.putExtra(Intent.EXTRA_TEXT, networkLog.toString());
                 sendIntent.setType("text/plain");
-                startActivity(Intent.createChooser(sendIntent,"Share with"));
+                startActivity(Intent.createChooser(sendIntent, "Share with"));
             }
         });
-
+        copyResponse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ClipboardManager clipboard = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+                ClipData clip = ClipData.newPlainText("label", response.getText().toString());
+                clipboard.setPrimaryClip(clip);
+                Toast.makeText(getApplicationContext(), "Response copied to clipboard", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
